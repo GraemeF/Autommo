4617e1dba76a68cf576eaabe7fa786d275c173e7
Graeme Foster <graemef@gmail.com>
2010-12-04 18:14:33 +0000

    Added code to run the command line runner.

--- EndToEndTests/Autommo.EndToEndTestEntities/AutommoServer.cs
+++ EndToEndTests/Autommo.EndToEndTestEntities/AutommoServer.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Autommo.EndToEndTestEntities
 {
     public class AutommoServer : IDisposable
     {
+        private readonly Process _process;
+#if DEBUG
+        private const string Configuration = "Debug";
+#else
+        private const string Configuration = "Release";
+#endif
+        private const string ServerFolder = @"..\..\..\Autommo.Console\bin\" + Configuration;
+        private const string ServerPath = ServerFolder + @"\Autommo.Console.exe";
+
         #region IDisposable Members
 
         public void Dispose()
@@ -19,8 +29,14 @@ namespace Autommo.EndToEndTestEntities
             Dispose(false);
         }
 
+        public AutommoServer()
+        {
+            _process = Process.Start(ServerPath, "/port=8099");
+        }
+
         private void Dispose(bool disposing)
         {
+            _process.CloseMainWindow();
         }
     }
 }
