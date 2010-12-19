6d10d67fbe8ac5c6487446ffc932751856a2a1be
Graeme Foster <graemef@gmail.com>
2010-12-19 20:59:43 +0000

    Ignore warning that is incorrect due to MEF.

--- Autommo.Console/MefNancyModuleLocator.cs
+++ Autommo.Console/MefNancyModuleLocator.cs
@@ -7,8 +7,10 @@ namespace Autommo.Console
     [Export(typeof (INancyModuleLocator))]
     public class MefNancyModuleLocator : INancyModuleLocator
     {
+#pragma warning disable 649
         [ImportMany(typeof (NancyModule), AllowRecomposition = true)]
         private IEnumerable<NancyModule> _modules;
+#pragma warning restore 649
 
         #region INancyModuleLocator Members
 
