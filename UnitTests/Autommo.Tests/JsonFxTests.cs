faf4a13e78671d255a9daec65af2d174d14ace20
Graeme Foster <graemef@gmail.com>
2011-01-09 22:30:53 +0000

    Added a test to show JsonFx bug.

---
+++ UnitTests/Autommo.Tests/JsonFxTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using JsonFx.Json;
+
+    using Should.Fluent;
+
+    using Xunit;
+
+    public class JsonFxTests
+    {
+        [Fact]
+        public void Read__ReadsAList()
+        {
+            string json = "{\"Bars\":[{\"Baz\":\"Test\"}]}";
+
+            var result = new JsonReader().Read<Foo>(json);
+
+            result.Bars.Single().Baz.Should().Equal("Test");
+        }
+    }
+
+    public class Foo
+    {
+        public Bar[] Bars { get; set; }
+    }
+
+    public class Bar
+    {
+        public string Baz { get; set; }
+    }
+}
