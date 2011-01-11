350133c03b3aeb8532927c9f0d7ce32e56adcace
Graeme Foster <graemef@gmail.com>
2011-01-11 20:41:57 +0000

    Implement Equals and ToString on WorldPoint.

--- Autommo.Dto/WorldPoint.cs
+++ Autommo.Dto/WorldPoint.cs
@@ -7,5 +7,25 @@
         public decimal Y { get; set; }
 
         public decimal Z { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null ||
+                GetType() != obj.GetType())
+                return false;
+
+            var other = (WorldPoint)obj;
+            return other.X == X && other.Y == Y && other.Z == Z;
+        }
+
+        public override int GetHashCode()
+        {
+            return (X + Y + Z).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1},{2})", X, Y, Z);
+        }
     }
 }
